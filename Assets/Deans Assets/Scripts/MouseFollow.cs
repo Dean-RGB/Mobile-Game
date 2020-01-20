5d0a008bb8dd9c592605597c74e6412b14fec719
Dean <dean_giddy@hotmail.co.uk>
2020-01-20 22:36:48 +0000

    0.1.0 + 0.1.3: Merged Joes work and started experimenting with asset movement and placement in a mirrored environment

---
+++ Assets/Deans Assets/Scripts/MouseFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+
+    [SerializeField] private GameObject prefab;
+
+    public void Update ()
+    {
+        UpdateThisPosition ();
+
+        if (Input.GetMouseButtonDown(0))
+            CreateInstance();
+    }
+
+    private void UpdateThisPosition ()
+    {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition, Camera.MonoOrStereoscopicEye.Mono);
+        transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+    }
+
+    private void CreateInstance ()
+    {
+        Instantiate(prefab, transform.position, Quaternion.identity, transform.parent);
+        return;
+    }
+}
