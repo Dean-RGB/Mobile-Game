4450f3d354d6747d714d5df70e1ad3d7864d5b51
Martin <martinkorodi002@googlemail.com>
2020-02-03 20:50:38 +0100

    0.1.0 + 0.1.1: Started with a physics-jumping idea, will not continue, something like this has to be done in 2D.

---
+++ Assets/MartinAssets/CodeCollection/Move.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public static Move Instance { set; get; }
+
+    [Header("Image")]
+    public float Speed;
+    public float BoostSeconds;
+    public float BoostMagnitude;
+
+    public bool Boost;
+
+    private Rigidbody RigidBody;
+
+    Vector3 direction = new Vector3(1, 0, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        RigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MoveCharacter();
+    }
+
+    private void Update()
+    {
+        var speed = RigidBody.velocity.magnitude;
+        print(speed);
+    }
+
+
+    private void MoveCharacter()
+    {
+        if (!Boost && RigidBody.velocity.magnitude > 30)
+            return;
+
+        RigidBody.AddForce(direction * Speed, ForceMode.Force);
+    }
+
+    public IEnumerator BoostCharacter()
+    { 
+        var localBoostTimer = BoostSeconds;
+        Boost = true;
+
+        for (int i = 0; i < localBoostTimer; i++)
+        {         
+            while (localBoostTimer >= 0)
+            {
+                RigidBody.AddForce(direction * BoostMagnitude, ForceMode.Impulse);
+                localBoostTimer -= Time.smoothDeltaTime;
+                yield return null;
+            }
+        }
+
+        Boost = false;
+    }
+}
---
+++ Assets/MartinAssets/CodeCollection/TimedFiller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimedFiller : MonoBehaviour
+{
+    [Header("Image")]
+    public Image Image;
+
+    [Header("Wait fors")]
+    public float SecondsToWait;
+
+    public Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      button =  Image.GetComponent<Button>();
+    }
+
+    public void PressButton()
+    {  
+        Image.fillAmount = 0;
+        button.enabled = false;
+        StartCoroutine(Move.Instance.BoostCharacter());
+        StartCoroutine(FillAmmountIncrease());
+    }
+
+    private IEnumerator FillAmmountIncrease()
+    {
+        while (Image.fillAmount < 1.0f)
+        {
+            Image.fillAmount  += SecondsToWait / 100;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+        button.enabled = true;
+
+        yield break;
+    }
+}
